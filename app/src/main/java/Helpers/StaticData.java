d647229b3e199d30f8342d1b4ede98b6d410d776
SinghJitender <Jitu.singh95@gmail.com>
2018-05-13 22:45:58 +0530

    First Production Half Ready - HOME

---
+++ app/src/main/java/Helpers/StaticData.java
@@ -0,0 +1,17 @@
+package Helpers;
+
+import java.security.PrivateKey;
+
+/**
+ * Created by JitenderPal on 13-05-2018.
+ */
+
+public class StaticData {
+
+    public static String SHARED_PREF = "xpensemanager";
+    public static String DATABASE_NAME = "xpensemanager.db";
+    public static String MAIN_TABLE_NAME = "maintable";
+    public static String TITLE_TABLE = "titles";
+    public static String TAGS_TABLE = "tags";
+    public static String BILLS_TABLE = "bills";
+}
---
+++ app/src/main/java/com/jitenderpal/xpensemanager/ExpenseDetails.java
@@ -0,0 +1,13 @@
+package com.jitenderpal.xpensemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExpenseDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense_details);
+    }
+}
